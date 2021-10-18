53ce4cb7d387fb71b4aa130ffde0bfa5071399bd
Lisa Stillwell <lisa@renci.org>
2021-10-18 12:08:25 -0400

    Some initial playing around

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,19 @@
-window.alert("hello there Lisa!");
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+  // get robot name
+  var robotName = window.prompt("What is your robot's name?");
+  window.alert("Hello " + robotName);
+  console.log("Hello " + robotName);
+
+// more console log tests
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + robotName);
+
+//call the function
+//fight();
